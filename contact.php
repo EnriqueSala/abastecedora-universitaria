2c4d3a2935578dcbeb3218616536a340314d4e6c
Jorge <sala488@gmail.com>
2017-06-15 15:44:36 -0500

    Correción envío de correo

--- contact.php
+++ contact.php
@@ -3,5 +3,9 @@
     $subject = "From: htmlspecialchars($_POST['username']) <htmlspecialchars($_POST['subject'])>\r\n";
     $message = "Reply-To: htmlspecialchars($_POST['message']) \r\n";
     $headers = "Content-type: text\r\n";
-    mail($to, $subject,, $message, $headers);
+    if(mail($to, $subject,, $message, $headers){
+        echo "<h1> Envío exitoso</h1>"
+    }else{
+        echo <h1>Something went wrong</h1>
+    }
 ?>
